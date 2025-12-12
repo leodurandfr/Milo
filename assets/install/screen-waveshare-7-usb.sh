80aa682783c3c8907b8184e585ba818176cad5d5
leodurandfr <leo.durand@gmx.com>
2025-12-12 01:32:25 +0100

    fix(boot): waveshare 7 1024x600

--- assets/install/screen-waveshare-7-usb.sh
+++ assets/install/screen-waveshare-7-usb.sh
@@ -5,8 +5,13 @@
 source "$(dirname "${BASH_SOURCE[0]}")/boot-common.sh"
 
 # Paramètres cmdline.txt spécifiques HDMI
-BOOT_PARAMS_SCREEN="video=HDMI-A-1:1024x600@60D"
+BOOT_PARAMS_SCREEN=""
 
 # Paramètres config.txt spécifiques HDMI
 CONFIG_PARAMS_SCREEN="hdmi_force_hotplug=1
-hdmi_safe=0"
+hdmi_blanking=1
+hdmi_group=2
+hdmi_mode=87
+hdmi_cvt=1024 600 60 6 0 0 0
+framebuffer_width=1024
+framebuffer_height=600"
