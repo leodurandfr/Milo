6f06d6ef323450018b161c22b9b68758260479fd
leodurandfr <leo.durand@gmx.com>
2025-12-12 01:32:25 +0100

    fix(boot): waveshare 7 1024x600

--- assets/install/boot-common.sh
+++ assets/install/boot-common.sh
@@ -4,9 +4,9 @@
 
 # Paramètres cmdline.txt universels
 BOOT_PARAMS_COMMON="quiet splash plymouth.ignore-serial-consoles"
-BOOT_PARAMS_COMMON="$BOOT_PARAMS_COMMON console=tty3 loglevel=0"
+BOOT_PARAMS_COMMON="$BOOT_PARAMS_COMMON console=tty3 loglevel=0 consoleblank=0"
 BOOT_PARAMS_COMMON="$BOOT_PARAMS_COMMON logo.nologo vt.global_cursor_default=0"
-BOOT_PARAMS_COMMON="$BOOT_PARAMS_COMMON fbcon=map:2 vt.handoff=7"
+BOOT_PARAMS_COMMON="$BOOT_PARAMS_COMMON fbcon=map:99 vt.handoff=7"
 
 # Paramètres config.txt universels
 CONFIG_PARAMS_COMMON="disable_splash=1"
