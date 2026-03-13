b90e1274020f60c8c31c8cfd6b785ba7cae4fa88
leodurandfr <leo.durand@gmx.com>
2026-03-13 13:53:23 +0100

    feat(milo-client): add web-UI driven hardware configuration

--- milo-client/app/routes/__init__.py
+++ milo-client/app/routes/__init__.py
@@ -5,10 +5,12 @@ from routes.health import create_health_router
 from routes.snapclient import create_snapclient_router
 from routes.equalizer import create_equalizer_router
 from routes.app_update import create_app_update_router
+from routes.hardware import create_hardware_router
 
 __all__ = [
     "create_health_router",
     "create_snapclient_router",
     "create_equalizer_router",
-    "create_app_update_router"
+    "create_app_update_router",
+    "create_hardware_router",
 ]
